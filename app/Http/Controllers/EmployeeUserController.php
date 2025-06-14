0614be8d221cdd88f47bee77dd055d6be05399a5
SlanSaf <slansaf@yandex.ru>
2025-06-14 17:48:34 +0300

    Working with forms checkbox array

--- app/Http/Controllers/EmployeeUserController.php
+++ app/Http/Controllers/EmployeeUserController.php
@@ -5,21 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\EmployeeUser;
-
+use Illuminate\Support\Facades\Redirect;
 
 class EmployeeUserController extends Controller
 {
-    //
-    public function show($id)  {
+	//
+	public function show($id = null)
+	{
 
-        return view('employeeuser', ['employee' => $id ? Employee::where('id', $id)->first() : null]);
-        
-    }
+		return view('employeeuser', ['employee' => $id ? EmployeeUser::where('id', $id)->first() : null]);
+	}
 
-     public function store(Request $request)  {
+	public function store(Request $request)
+	{
 
-        $employee = new EmployeeUser($request->all());
-        $employee->department = serialize($request->input('department'));
-        $employee->save();
-    }
+		$employee = new EmployeeUser($request->all());
+		$employee->department = serialize($request->input('department'));
+		$employee->save();
+
+		return Redirect::route('show_employee', ['id' => $employee->id]);
+	}
 }
--- app/Models/EmployeeUser.php
+++ app/Models/EmployeeUser.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmployeeUser extends Model
 {
-    //
-    public $fillable = ['first_name', 'last_name', 'department'];
+	public $fillable = ['first_name', 'last_name', 'department'];
 
 }
