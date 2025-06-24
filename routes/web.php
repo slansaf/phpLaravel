c8aaf931cb3a2d7210eb636177891c86fe677a5c
Ruslan <slansaf@yandex.ru>
2025-06-24 14:12:50 +0300

    Implementation of authorization

--- routes/web.php
+++ routes/web.php
@@ -1,76 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\TestFormController;
-use App\Http\Controllers\EmployeeUserController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\UserResponseController;
-use App\Http\Controllers\PdfGeneratorController;
-use App\Events\NewsHidden;
-
 
 Route::get('/', function () {
-	return view('welcome');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::get('/users', [\App\Http\Controllers\UserController::class, 'showUsers']);
-
-Route::get('/test', [\App\Http\Controllers\SimpleController::class, 'test']);
-
-Route::get('/form', [\App\Http\Controllers\FormProcessor::class, 'index']);
-Route::post('/form', [\App\Http\Controllers\FormProcessor::class, 'store'])->name('store-form');
-
-Route::get('/home', function () {
-	return view('home', [
-		'name' => 'Иван',
-		'age' => 30,
-		'position' => 'Разработчик',
-		'address' => 'Москва, Россия'
-	]);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/contacts', function () {
-	return view('contacts', [
-		'address' => 'Москва, Россия',
-		'post_code' => '101000',
-		'email' => 'example@example.com',
-		'phone' => '+7 (123) 456-78-90'
-	]);
-});
-
-Route::get('/employee', [EmployeeController::class, 'index'])->name('employee.index');
-Route::post('/employee/store', [EmployeeController::class, 'store'])->name('employee.store');
-Route::get('/employee/{id}', [EmployeeController::class, 'update'])->name('employee.update');
-
-Route::get('/form', [TestFormController::class, 'displayForm'])->name('show_form');
-Route::post('/form', [TestFormController::class, 'postForm'])->name('post_form');
-
-
-Route::post('/employee-user', [EmployeeUserController::class, 'store'])->name('store_employee');
-Route::get('/employee-user/{id?}', [EmployeeUserController::class, 'show'])->name('show_employee');
-
-Route::post('/add-books', [BookController::class, 'store'])->name('store_books');
-Route::get('/add-books', [BookController::class, 'show'])->name('show_books');
-
-Route::get('/users', [UserResponseController::class, 'index']);
-Route::get('/users/{id?}', [UserResponseController::class, 'show']);
-Route::post('/users', [UserResponseController::class, 'store']);
-Route::get('/resume/{id?}', [PdfGeneratorController::class, 'index']);
-
-Route::get('/news/create-test', function () {
-    $news = new \App\Models\News();
-    $news->title = 'Test News Title';
-    $news->body = 'This is a test news body.';
-    $news->save();
-    return 'Test news created!';
-});
-
-Route::get('/news/{id}/hide', function ($id) {
-    $news = \App\Models\News::findOrFail($id);
-    $news->hidden = true;
-    $news->save();
-    NewsHidden::dispatch($news);
-    return 'News hidden!';
-});
+require __DIR__.'/auth.php';
