9b577be19c1bf2814eb44ba3e3232e8e31ecaeb1
Ruslan <slansaf@yandex.ru>
2025-07-21 09:51:12 +0300

    Homework for the lesson Implementation of Authorization has been completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 
 class UserController extends Controller
 {
     //
-    public function showUsers() {
-       $users = DB::connection('mysql')->table('Users')->select(['first_name', 'last_name', 'birth_year'])->get();
-       return view('users', ['users' => $users]);
+    // public function showUsers() {
+    //    $users = DB::connection('mysql')->table('Users')->select(['first_name', 'last_name', 'birth_year'])->get();
+    //    return view('users', ['users' => $users]);
+    // }
+
+    public function index(){
+        $this->authorize('view-any', User::class);
+        $users = User::all();
+     
+    }
+
+    public function show(User $user){
+          $this->authorize('view', $user);
+        return $user;
     }
 }
---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Policies\UserPolicy;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [
+        User::class => UserPolicy::class,
+    ];
+
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Gate::define('view-users', function (User $user) { 
+            return $user->isAdmin(); 
+        });
+   }
+}
